7731ff174ccdc8d7c812648c82ccce5cd5bbab69
koustub manchiraju <koustub.247@gmail.com>
2020-02-19 12:09:12 -0500

    delete todo/bg color

--- calendar.js
+++ calendar.js
@@ -7,47 +7,72 @@ var buttonSixthPressed = document.getElementById('btn6');
 var buttonSeventhPressed = document.getElementById('btn7');
 var buttonEigthPressed = document.getElementById('btn8');
 var buttonNinthPressed = document.getElementById('btn9');
+
+var UbuttonOnePressed = document.getElementById('btn21');
+var UbuttonSecondPressed = document.getElementById('btn22');
+var UbuttonThirdPressed = document.getElementById('btn23');
+var UbuttonFourthPressed = document.getElementById('btn24');
+var UbuttonFifthPressed = document.getElementById('btn25');
+var UbuttonSixthPressed = document.getElementById('btn26');
+var UbuttonSeventhPressed = document.getElementById('btn27');
+var UbuttonEigthPressed = document.getElementById('btn28');
+var UbuttonNinthPressed = document.getElementById('btn29');
+
+
 var dt = moment().format('dddd, MMMM Do ');
 document.getElementById('dayDate').innerHTML = `${dt}`;
 console.log(`day and date${dt}`);
-var time = moment().format('h:mm');
+var time = moment().format('hh');
 console.log(`${time}`);
 var fhtd = [];
 var todolist =[];
 
-    
+
 
 // Taking input values 
-var fh = document.getElementById('firsthour');
-var sh = document.getElementById('secondhour');
-var th = document.getElementById('thirdhour');
-var frh = document.getElementById('fourthhour');
-var fih = document.getElementById('fifthhour');
-var sih = document.getElementById('sixthhour');
-var svh = document.getElementById('seventhhour');
-var eh = document.getElementById('eighthhour');
-var nh = document.getElementById('ninthhour');
+var fh = document.getElementById('1hour');
+var sh = document.getElementById('2hour');
+var th = document.getElementById('3hour');
+var frh = document.getElementById('4hour');
+var fih = document.getElementById('5hour');
+var sih = document.getElementById('6hour');
+var svh = document.getElementById('7hour');
+var eh = document.getElementById('8hour');
+var nh = document.getElementById('9hour');
 
 
-var then = "12:00";
-var ms = moment(time,"HH:mm").diff(moment(then,"HH:mm"));
-var d = moment.duration(ms).humanize();
-var s = d.format;
+var ms = moment().format("h");
 
 
-
-
-
-
+for (i=1;i<=9;i++){
+    console.log('i m entering loop');
+    var timeToCheck = document.getElementById(`h${i}`);
+    var inputByHour = document.getElementById(`${i}hour`);
+    var timeOfDay =timeToCheck.innerText;
+    var tod = timeOfDay.slice(0,2);
+    console.log(`${tod}`);
+    console.log(`${ms}`);
+    if(tod < ms){
+        inputByHour.style.backgroundColor='red';
+        console.log('its less then');
+    }else if(tod > ms){
+        inputByHour.style.backgroundColor='green';
+        console.log('its greather than');
+    }else if(tod == ms){
+        inputByHour.style.backgroundColor='grey';
+        console.log('its equal ');
+    }
+}
 
 var todo;
 var valueId;
-console.log(`${d}`);
+
 
 buttonOnePressed.addEventListener('click',function(){
     console.log('button is pressed');
      todo = fh.value;
      valueId = 'button1';
+    
     todolist.push({valueId,todo});
     console.log(`${todolist[0]}`);
     localStorage.setItem('todolist',JSON.stringify(todolist));
@@ -116,144 +141,140 @@ buttonNinthPressed.addEventListener('click',function(){
     console.log(`${todolist[8]}`);
     localStorage.setItem('todolist',JSON.stringify(todolist));
 });
+UbuttonOnePressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    fh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonSecondPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    sh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonThirdPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    th.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
 
-if(window.localStorage.length !== 0 ){
-    debugger;
+UbuttonFourthPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    frh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonFifthPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    fih.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonSixthPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    sih.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+
+UbuttonSeventhPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    svh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonEigthPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    eh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+UbuttonNinthPressed.addEventListener('click', function () {
+    console.log('button is pressed');
+    nh.value = "";
+    var todolist = JSON.parse(localStorage.getItem('todolist'));
+    delete todolist[0];
+    var rhtd = todolist.filter(Boolean);
+    localStorage.setItem('todolist', JSON.stringify(rhtd));
+    console.log(`key deleted`);
+});
+if(window.localStorage.length !== 0 )
+{
     window.onload = showTodo1(); 
-    }
+}
+
+   
     
 function showTodo1(){
 
-    fhtd = JSON.parse(localStorage.getItem('todolist'));
-    for(var i=0;i<fhtd.length;i++){
-        //debugger;
-        console.log(`${fhtd[0].valueId}`);
-    var check = fhtd[i].valueId;
-    switch (check){
-        
-        case 'button1':
-            console.log('has a value');
-            debugger;
-            fh.value = fhtd[i].todo;
-            break;
-        case 'button2':
-            sh.value = fhtd[i].todo;
-            break;
-        case 'button3':
-            th.value = fhtd[i].todo;
-            break;
-        case 'button4':
-            frh.value = fhtd[i].todo;
-            break;
-        case 'button5':
-            fih.value = fhtd[i].todo;
-            break;
-        case 'button6':
-            sih.value = fhtd[i].todo;
-            break;
-        case 'button7':
-            svh.value = fhtd[i].todo;
-            break;
-        case 'button8':
-            eh.value = fhtd[i].todo;
-            break;
-        case 'button9':
-            nh.value = fhtd[i].todo;
-            break;
-        default:
-            console.log('sorry something is wrong ');
-        
-        
+    var fhtd = JSON.parse(localStorage.getItem('todolist'));    
+    for(var i=0;i<fhtd.length;i++)
+    {
+       
+        var check = fhtd[i].valueId;
+        console.log(`${check}`);
+        switch (check) {
+
+            case 'button1':
+                console.log('has a value');
+
+                fh.value = fhtd[i].todo;
+                break;
+            case 'button2':
+                sh.value = fhtd[i].todo;
+                break;
+            case 'button3':
+                th.value = fhtd[i].todo;
+                break;
+            case 'button4':
+                frh.value = fhtd[i].todo;
+                break;
+            case 'button5':
+                fih.value = fhtd[i].todo;
+                break;
+            case 'button6':
+                sih.value = fhtd[i].todo;
+                break;
+            case 'button7':
+                svh.value = fhtd[i].todo;
+                break;
+            case 'button8':
+                eh.value = fhtd[i].todo;
+                break;
+            case 'button9':
+                nh.value = fhtd[i].todo;
+                break;
+            default:
+                console.log('sorry something is wrong ');
+
+        }
     }
 }
-}
 
-if ('9:00' > time){
-    fh.style.backgroundColor='grey';
-}
-else if('9:00' < time){
-    fh.style.backgroundColor='grey';
-}
-else{
-    fh.style.backgroundColor='green';
-}
-
-if ('10:00' > time){
-    sh.style.backgroundColor='grey';
-}
-else if('10:00' < time){
-    sh.style.backgroundColor='grey';
-}
-else{
-    sh.style.backgroundColor='green';
-}
-
-if ('11:00' > time){
-    th.style.backgroundColor='grey';
-}
-else if('11:00' < time){
-    th.style.backgroundColor='grey';
-}
-else{
-    th.style.backgroundColor='green';
-}
-
-if ('12:00' > time){
-    frh.style.backgroundColor='grey';
-}
-else if('12:00' < time){
-    frh.style.backgroundColor='grey';
-}
-else{
-    frh.style.backgroundColor='green';
-}
-
-if ('13:00' > time){
-    fih.style.backgroundColor='grey';
-}
-else if('13:00' < time){
-    fih.style.backgroundColor='grey';
-}
-else{
-    fih.style.backgroundColor='green';
-}
-
-if ('14:00' > time){
-    sih.style.backgroundColor='grey';
-}
-else if('14:00' < time){
-    sih.style.backgroundColor='grey';
-}
-else{
-    sih.style.backgroundColor='green';
-}
-
-if ('15:00' > time){
-    svh.style.backgroundColor='grey';
-}
-else if('15:00' < time){
-    svh.style.backgroundColor='grey';
-}
-else{
-    svh.style.backgroundColor='green';
-}
-
-if ('16:00' > time){
-    eh.style.backgroundColor='grey';
-}
-else if('16:00' < time){
-    eh.style.backgroundColor='grey';
-}
-else{
-    eh.style.backgroundColor='green';
-}
-
-if ('17:00' > time){
-    nh.style.backgroundColor='grey';
-}
-else if('17:00' < time){
-    nh.style.backgroundColor='grey';
-}
-else{
-    nh.style.backgroundColor='green';
-}
+ 
